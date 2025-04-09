a6bac228f315557eacf4256fca7df4f49944ddfe
Igor Latii <igor.latii@outlook.com>
2025-04-10 00:12:18 +0300

    Changed DB structure (updated question_answer table, dropped user_question table)

--- admin_panel/adminapp/admin.py
+++ admin_panel/adminapp/admin.py
@@ -2,30 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import QuestionAnswer, UserQuery, PredefinedResponse
+from .models import QuestionAnswer, PredefinedResponse
 
 
 @admin.register(QuestionAnswer)
 class QuestionAnswerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question_summary', 'language', 'created_at', 'updated_at')
-    search_fields = ('question', 'answer')
-    list_filter = ('language', )
+    list_display = ('id', 'question_summary', 'answer', 'language', 'processed', 'created_at', 'updated_at')
+    search_fields = ('question', 'processed')
+    list_filter = ('language', 'processed')
     ordering = ('-updated_at',)
 
     def question_summary(self, obj):
         return obj.question[:50] + "..." if len(obj.question) > 50 else obj.question
     question_summary.short_description = "Question"
-
-@admin.register(UserQuery)
-class UserQueryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_id', 'query_summary', 'processed', 'created_at')
-    search_fields = ('query', )
-    list_filter = ('processed', )
-    ordering = ('-created_at',)
-
-    def query_summary(self, obj):
-        return obj.query[:50] + "..." if len(obj.query) > 50 else obj.query
-    query_summary.short_description = "User Query"
 
 @admin.register(PredefinedResponse)
 class PredefinedResponseAdmin(admin.ModelAdmin):
--- admin_panel/adminapp/models.py
+++ admin_panel/adminapp/models.py
@@ -6,7 +6,8 @@ from django.db import models
 class QuestionAnswer(models.Model):
     question = models.TextField()
     answer = models.TextField()
-    language = models.CharField(max_length=10, choices=[('ru', 'Russian'), ('ro', 'Romanian'), ('en', 'English')])
+    language = models.CharField(max_length=10, choices=[('ru', 'Russian'), ('ro', 'Romanian'), ('eng', 'English')])
+    processed = models.BooleanField(default=False)
     embedding = models.TextField(blank=True, null=True)  # JSON-vector
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -16,19 +17,6 @@ class QuestionAnswer(models.Model):
 
     def __str__(self):
         return f"Q: {self.question[:50]}..."
-
-class UserQuery(models.Model):
-    user_id = models.BigIntegerField(null=True, blank=True)
-    query = models.TextField()
-    detected_keywords = models.TextField(null=True, blank=True)
-    processed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'user_queries'
-
-    def __str__(self):
-        return f"User {self.user_id}: {self.query[:50]}..."
 
 class PredefinedResponse(models.Model):
     command = models.CharField(max_length=50)
